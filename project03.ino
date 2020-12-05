9baf88bbcb827f18dd003b5ea71288a4db43b1f5
dougbrown1048 <70710191+dougbrown1048@users.noreply.github.com>
2020-12-05 13:17:16 -0800

    Add files via upload

---
+++ project03.ino
@@ -0,0 +1,72 @@
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *myMotor = AFMS.getMotor(4);
+
+
+int trigPin = 6;    // TRIG pin
+int echoPin = 5;    // ECHO pin
+
+float duration_us, distance_cm;
+
+void setup() {
+  // begin serial port
+  Serial.begin (9600);
+
+  AFMS.begin();  // create with the default frequency 1.6KHz
+
+  // configure the trigger pin to output mode
+  pinMode(trigPin, OUTPUT);
+  // configure the echo pin to input mode
+  pinMode(echoPin, INPUT);
+
+  // Set the speed to start, from 0 (off) to 255 (max speed)
+  myMotor->setSpeed(150);
+  myMotor->run(FORWARD);
+  // turn on motor
+  myMotor->run(RELEASE);
+}
+
+void loop() {
+  int i;
+  int j;
+  
+  // generate 10-microsecond pulse to TRIG pin
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // measure duration of pulse from ECHO pin
+  duration_us = pulseIn(echoPin, HIGH);
+
+  // calculate the distance
+  distance_cm = 0.017 * duration_us;
+
+   myMotor->run(FORWARD);
+   if (distance_cm<10) {
+    myMotor->setSpeed(0);
+   }
+   else if (distance_cm>145) {
+    myMotor->setSpeed(255);
+   }
+   else if (10<=distance_cm<50) {
+    i=(distance_cm-10)*4;
+    myMotor->setSpeed(i);
+   }
+   else if (50<=distance_cm<=145) {
+    j=distance_cm+110;
+    myMotor->setSpeed(i);
+   }
+
+  // print the value to Serial Monitor
+  Serial.print("distance: ");
+  Serial.print(distance_cm);
+  Serial.println(" cm");
+
+  delay(10);
+}
